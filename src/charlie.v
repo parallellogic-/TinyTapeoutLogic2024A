f59c5172996632bebc5cd882007edff1aad0167b
ParallelLogic- <parallellogic@gmail.com>
2024-11-02 19:24:06 -0700

    charlie module test

---
+++ src/charlie.v
@@ -0,0 +1,54 @@
+
+module charlie (
+  input  wire       clk,      // clock
+  input wire [63:0] memory_frame_buffer,
+  input wire [5:0] frame_done_index,
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+  output wire is_frame_done
+);
+  
+reg[5:0] charlie_index;
+wire [2:0] row_index;
+  wire [2:0] col_index;
+  //wire is_diagonal;
+  wire is_on;
+  wire [7:0] memory [0:7];
+  reg [7:0] uio_out_reg;
+  reg [7:0] uio_oe_reg;
+  
+  assign uio_out=uio_out_reg;
+  assign uio_oe=uio_oe_reg;
+  
+  assign memory[0] = memory_frame_buffer[7:0];
+  assign memory[1] = memory_frame_buffer[15:8];
+  assign memory[2] = memory_frame_buffer[23:16];
+  assign memory[3] = memory_frame_buffer[31:24];
+  assign memory[4] = memory_frame_buffer[39:32];
+  assign memory[5] = memory_frame_buffer[47:40];
+  assign memory[6] = memory_frame_buffer[55:48];
+  assign memory[7] = memory_frame_buffer[63:56];
+  
+  assign col_index=charlie_index[2:0];
+  assign row_index=charlie_index[5:3];
+  
+  assign is_frame_done=frame_done_index==charlie_index;
+  
+  //assign is_diagonal = row_index == col_index;//if on diagonal, do nothing
+  assign is_on=memory[row_index][col_index];//fetch state of this LED
+  
+  always @(posedge clk)
+  begin
+    charlie_index <= charlie_index+1;
+    uio_oe_reg<=8'b0;
+    uio_out_reg<=8'b0;
+    //if(!is_diagonal && is_on) begin
+      uio_oe_reg[row_index]<=is_on;
+      uio_oe_reg[col_index]<=is_on;
+      
+      uio_out_reg[row_index]<=1;
+      uio_out_reg[col_index]<=0;
+   // end
+  end
+  
+endmodule
--- src/project.v
+++ src/project.v
@@ -4,6 +4,8 @@
  */
 
 `default_nettype none
+`include "charlie.v"
+`include "spi_slave.v"
 
 module tt_um_parallellogic_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -16,12 +18,46 @@ module tt_um_parallellogic_top (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  parameter MEMORY_COUNT=12;
+  reg [7:0] memory [0:(MEMORY_COUNT-1)];
+  wire is_frame_done;
+  wire [5:0] frame_done_index;
+  wire [(8*8-1):0] memory_frame_buffer;//flatten to work with yosys expectations of 1D lists
+  
+  assign frame_done_index={memory[5][5],memory[4][4],memory[3][3],memory[2][2],memory[1][1],memory[0][0]};
+  assign memory_frame_buffer={memory[7], memory[6], memory[5], memory[4], memory[3], memory[2], memory[1], memory[0]};
+  
+  always @(posedge clk)
+  begin
+    if(!rst_n) begin
+      memory[0]<=8'b0;
+      memory[1]<=8'b0;
+      memory[2]<=8'b0;
+      memory[3]<=8'b0;
+      memory[4]<=8'b0;
+      memory[5]<=8'b0;
+      memory[6]<=8'b0;
+      memory[7]<=8'b0;
+      memory[8]<=8'b0;
+      memory[9]<=8'b0;
+      memory[10]<=8'b0;
+      memory[11]<=8'b0;
+     
+    end
+  end
+  
+  charlie cha(
+  clk,      // clock
+    memory_frame_buffer,
+    frame_done_index,
+    uio_out,  // IOs: Output path
+    uio_oe,
+    is_frame_done
+  );
+  
+  
+  assign uo_out =8'b0;
+  
+  wire _unused = &{ena, clk, rst_n, 1'b0,uio_in,is_frame_done,ui_in};
 
 endmodule
